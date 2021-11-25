d851b7d575ccf2376a34c2882feea2506125e0b6
Mochamad Rafli Ramadhan <mochamadrafli2018@mail.ugm.ac.id>
2021-11-25 12:14:02 +0700

    Update

--- src/pages/Guide.js
+++ src/pages/Guide.js
@@ -12,34 +12,35 @@ export default function Guide () {
           </p>
           <hr/>
           <p className='my-2'>
-            Ada 26 penyakit mata yang ada pada database sistem diantaranya :
-            1.) Ulkus Kornea
-            2.) Keratokonus
-            3.) Kalazion
-            4.) Blefaritis
-            5.) Hordeolum (Stye)
-            6.) Konjungtivitis
-            7.) Trakoma
-            8.) Abalso Retina
-            9.) Retinopati Diabetikum
-            10.) Glaukoma
-            11.) Katarak
-                12.) Uveitis
-                13.) Selulitis Orbitalis
-                14.) Eksoftalmus
-                15.) Keratitis Pungtata Superfisialis
-                16.) Alergi Mata Merah
-                17.) Endoftalmitis
-                18.) Trombosis Sinus Kavernosus
-                19.) Optic Neuritis
-                20.) Dakrioritis
-                21.) Degenerasi Makula
-                22.) Episkleritis
-                23.) Infeksi Herpes Simpleks Kornea
-                24.) Infeksi Herpes Zoster Kornea
-                25.) Keratitis Ulserativa Perifer
-                28.) Oftalmia Neonatorum
+            Pada chatbot sistem pakar ini, terdapat 26 data penyakit mata beserta gejalanya diantaranya :
           </p>
+          <p className='my-1'>1.) Ulkus Kornea</p>
+          <p className='my-1'>2.) Keratokonus</p>
+          <p className='my-1'>3.) Kalazion</p>
+          <p className='my-1'>4.) Blefaritis</p>
+          <p className='my-1'>5.) Hordeolum (Stye)</p>
+          <p className='my-1'>6.) Konjungtivitis</p>
+          <p className='my-1'>7.) Trakoma</p>
+          <p className='my-1'>8.) Abalso Retina</p>
+          <p className='my-1'>9.) Retinopati Diabetikum</p>
+          <p className='my-1'>10.) Glaukoma</p>
+          <p className='my-1'>11.) Katarak</p>
+          <p className='my-1'>12.) Uveitis</p>
+          <p className='my-1'>13.) Selulitis Orbitalis</p>
+          <p className='my-1'>14.) Eksoftalmus</p>
+          <p className='my-1'>15.) Keratitis Pungtata Superfisialis</p>
+          <p className='my-1'>16.) Alergi Mata Merah</p>
+          <p className='my-1'>17.) Endoftalmitis</p>
+          <p className='my-1'>18.) Trombosis Sinus Kavernosus</p>
+          <p className='my-1'>19.) Optic Neuritis</p>
+          <p className='my-1'>20.) Dakrioritis</p>
+          <p className='my-1'>21.) Degenerasi Makula</p>
+          <p className='my-1'>22.) Episkleritis</p>
+          <p className='my-1'>23.) Infeksi Herpes Simpleks Kornea</p>
+          <p className='my-1'>24.) Infeksi Herpes Zoster Kornea</p>
+          <p className='my-1'>25.) Keratitis Ulserativa Perifer</p>
+          <p className='my-1'>28.) Oftalmia Neonatorum</p>
+          
           <p className='my-2'>Untuk melakukan skrining penyakit mata ketikkan : mulai,  tes,  test atau skrining pada input chat.</p>
           <p className='my-2'>Atau bisa juga tekan tombol <strong>mulai</strong> berwarna biru yang ada di bawah input chat.</p>
           <p className='my-2'>Jawab setiap pertanyaan dengan <strong>y (ya)</strong> atau <strong>t (tidak)</strong>.</p>
@@ -56,7 +57,7 @@ export default function Guide () {
           <p className='my-2'>9) selamat tinggal, sampai jumpa, sampai jumpa lagi</p>
           <p className='my-2'>10) covid, corona </p>
           <p className='my-2'>Untuk menyimpan hasil skrining anda ke komputer lokal, anda bisa menekan CTRL + P.</p>
-          <p className='my-2'>Perlu diketahui bahwa hasil skrining dari sistem pakar tersebut <strong>tidak bisa</strong> dijadikan sebagai keputusan akhir diagnosis penyakit mata anda.</p>
+          <p className='my-2'>Perlu diketahui bahwa hasil skrining dari sistem pakar ini <strong>tidak bisa</strong> dijadikan sebagai keputusan akhir diagnosis penyakit mata anda.</p>
           <p className='my-2'>Hasil skrining tersebut bisa anda gunakan untuk konsultasi dengan dokter spesialis mata untuk diagnosis lebih lanjut.</p>
           {/*<InferenceMachineCopy/>*/}
         </div>
