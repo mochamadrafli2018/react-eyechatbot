36bd43be750b69cbb60423e4f1b61eb1d3270f75
Mochamad Rafli Ramadhan <mochamadrafli2018@mail.ugm.ac.id>
2021-11-25 10:47:14 +0700

    Update

--- src/function-components/AddChat.js
+++ src/function-components/AddChat.js
@@ -39,6 +39,6 @@ export default function addChat(input, reply) {
   // Fake delay to seem 'real'
   setTimeout(() => {
     botText.innerHTML = 
-    `<i class="bi bi-robot"></i> ${reply} <span class='ya-tidak'>y/t</span>`
+    `<i class="bi bi-robot"></i> ${reply}`
   }, 300)
 }
