4473c2f69ee10db5d0ee9c62dc2de6bf6adcb960
Mochamad Rafli Ramadhan <69032682+mochamadrafli2018@users.noreply.github.com>
2022-01-25 08:24:56 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import ChatbotDashboard from './pages/ChatbotDashboard';
 import Dashboard from './pages/Dashboard';
 import FormTesting from './pages/FormTesting';
 import Guide from './pages/Guide';
-import QnA from './pages/QnA';
 
 class App extends React.Component {
   render(){
@@ -22,7 +21,6 @@ class App extends React.Component {
           <Route exact path='/chatbot' component={ChatbotDashboard} />
           <Route exact path='/chatbot/form' component={FormTesting} />
           <Route exact path='/chatbot/guide' component={Guide} />
-          <Route exact path='/chatbot/qna' component={QnA} />
           <Route exact path='/vue' component={() => window.location = 'https://vue-eyescreening.netlify.app'}/>
           {/* exact path='/' must be the last to write*/}
           <Route exaxt path='/' component={Dashboard} />
